35f5526741fbd941e75a060e0a7a0dd97a12b34f
Marek71cz <marek.71cz@gmail.com>
2020-08-05 17:09:17 +0200

    Version change

--- sc2-integrate.user.js
+++ sc2-integrate.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            SC2-integrate
 // @license         MIT
-// @version         3.0
+// @version         3.1
 // @downloadURL     https://raw.githubusercontent.com/Marek71cz/sc2-integrate/master/sc2-integrate.user.js
 // @updateURL       https://raw.githubusercontent.com/Marek71cz/sc2-integrate/master/sc2-integrate.user.js
 // @description     Integrace SC2 do CSFD, IMDB a TRAKT.TV.
